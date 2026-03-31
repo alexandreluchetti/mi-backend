3b34d6b704936b5c6e77441c87ead7ddb525dca0
alexandreluchetti <luchetti.92@gmail.com>
2026-03-31 16:12:48 -0300

    Inicializando projeto

---
+++ src/test/java/br/com/alexandreluchetti/mibackend/MiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alexandreluchetti.mibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
