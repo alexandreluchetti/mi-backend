d4c5b37f78a582562df44a064c6634aacda73845
alexandreluchetti <luchetti.92@gmail.com>
2026-04-01 10:32:08 -0300

    Criando ArquivoUseCase

---
+++ src/main/java/br/com/alexandreluchetti/mibackend/core/usecase/ArquivoUseCase.java
@@ -0,0 +1,21 @@
+package br.com.alexandreluchetti.mibackend.core.usecase;
+
+import br.com.alexandreluchetti.mibackend.entrypoint.dto.ProgressoResponseDTO;
+import br.com.alexandreluchetti.mibackend.entrypoint.dto.ResultadoResponseDTO;
+import br.com.alexandreluchetti.mibackend.entrypoint.dto.UploadResponseDTO;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface ArquivoUseCase {
+
+    /**
+     * Valida o cabeçalho do arquivo e, se válido, registra o upload e dispara
+     * o processamento em background. O InputStream é lido apenas uma vez.
+     */
+    UploadResponseDTO upload(MultipartFile file) throws IOException;
+
+    ProgressoResponseDTO consultarProgresso(String id);
+
+    ResultadoResponseDTO consultarResultado(String id);
+}
