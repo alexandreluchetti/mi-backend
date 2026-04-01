6c7fcea0a1202f87f26979df0b7ec12576c3eadd
alexandreluchetti <luchetti.92@gmail.com>
2026-04-01 18:17:43 -0300

    Adicionando exceptionHandling para securityFilter

--- src/main/java/br/com/alexandreluchetti/mibackend/config/shared/SecurityConfig.java
+++ src/main/java/br/com/alexandreluchetti/mibackend/config/shared/SecurityConfig.java
@@ -4,11 +4,13 @@ import br.com.alexandreluchetti.mibackend.core.model.RoleEnum;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -36,13 +38,13 @@ public class SecurityConfig {
                                 "/swagger-resources/**",
                                 "/webjars/**"
                         ).permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/arquivos/upload")
-                                .hasRole(RoleEnum.ENVIO.name())
-                        .requestMatchers(HttpMethod.GET, "/api/arquivos/*/progresso")
-                                .hasAnyRole(RoleEnum.ENVIO.name(), RoleEnum.CONSULTA.name())
-                        .requestMatchers(HttpMethod.GET, "/api/arquivos/*/resultado")
-                                .hasRole(RoleEnum.CONSULTA.name())
+                        .requestMatchers(HttpMethod.POST, "/api/arquivos/upload").hasRole(RoleEnum.ENVIO.name())
+                        .requestMatchers(HttpMethod.GET, "/api/arquivos/*/progresso").hasAnyRole(RoleEnum.ENVIO.name(), RoleEnum.CONSULTA.name())
+                        .requestMatchers(HttpMethod.GET, "/api/arquivos/*/resultado").hasRole(RoleEnum.CONSULTA.name())
                         .anyRequest().authenticated()
+                )
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 )
                 .addFilterBefore(staticTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
